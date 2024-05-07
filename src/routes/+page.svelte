bb113cd65dc8e96fa6f0eaad5bf4a2930814b482
Claudiu Macovei <makowey@gmail.com>
2024-05-07 08:32:18 +0300

    git source

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,6 @@
 	import WebixSample from '$lib/WebixSample.svelte';
 </script>
 
-<h1 class="text-2xl m-2">PoC SvelteKit & Webix</h1>
+<h1 class="text-2xl m-2">PoC SvelteKit & Webix (with Tailwind)</h1>
+<a href="https://github.com/makowey/svelte-webix" class="anchor-link font-bold m-2">Git source</a>
 <WebixSample />
