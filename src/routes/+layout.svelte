c5795c02865e4ec05247173adff741cf9dc5d551
Claudiu Macovei <makowey@gmail.com>
2024-05-07 08:24:32 +0300

    setup tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import "../app.css";
 	import LibLoader from '$lib/LibLoader.svelte';
 
 	function onLoaded() {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
 	import WebixButton from '$lib/WebixButton.svelte';
 </script>
 
-<h1>PoC SvelteKit & Webix</h1>
+<h1 class="text-2xl m-2">PoC SvelteKit & Webix</h1>
 <WebixButton />
